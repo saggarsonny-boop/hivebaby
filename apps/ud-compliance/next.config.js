aad63d59a6d914c7dc6ab4eaf7f66ddc3b336e1e
Sonny Saggar <sonny@hive.baby>
2026-05-10 13:34:58 -0500

    fix: Remove broken next-pwa wrapper causing Vercel Webpack build crashes

--- apps/ud-compliance/next.config.js
+++ apps/ud-compliance/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true
-});
-
 const nextConfig = {
   reactStrictMode: true,
 };
-
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
