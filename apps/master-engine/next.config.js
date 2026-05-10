59e22b982832b42d6bec0349e6157bbc123c4e4a
Sonny Saggar <sonny@hive.baby>
2026-05-10 15:00:51 -0500

    fix: Hardcode Clerk keys in nextConfig to bypass Vercel env cache issues

--- apps/master-engine/next.config.js
+++ apps/master-engine/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_Y2hvaWNlLWRhc3NpZS02NS5jbGVyay5hY2NvdW50cy5kZXYk",
+    CLERK_SECRET_KEY: "sk_test_NZX6GkWzVeHuY4WKI8Fw8Qlh43i17pFF9la6Rx6AWB"
+  }
 };
 module.exports = nextConfig;
